4444b46f22a10a51a04d92c6ad35fa68951f6386
Patrick Knueppel <pknueppel@comcast.net>
2025-05-02 18:06:13 -0400

    updated for debugging

--- init.lua
+++ init.lua
@@ -852,7 +852,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
@@ -882,3 +882,69 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+local mason_dap = require 'mason-nvim-dap'
+local dap = require 'dap'
+local ui = require 'dapui'
+local dap_virtual_text = require 'nvim-dap-virtual-text'
+
+-- Dap Virtual Text
+dap_virtual_text.setup()
+
+mason_dap.setup {
+  ensure_installed = { 'cppdbg' },
+  automatic_installation = true,
+  handlers = {
+    function(config)
+      require('mason-nvim-dap').default_setup(config)
+    end,
+  },
+}
+
+-- Configurations
+dap.configurations = {
+  c = {
+    {
+      name = 'Launch file',
+      type = 'cppdbg',
+      request = 'launch',
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+      cwd = '${workspaceFolder}',
+      stopAtEntry = false,
+      MIMode = 'lldb',
+    },
+    {
+      name = 'Attach to lldbserver :1234',
+      type = 'cppdbg',
+      request = 'launch',
+      MIMode = 'lldb',
+      miDebuggerServerAddress = 'localhost:1234',
+      miDebuggerPath = '/usr/bin/lldb',
+      cwd = '${workspaceFolder}',
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+    },
+  },
+}
+
+-- Dap UI
+
+ui.setup()
+
+vim.fn.sign_define('DapBreakpoint', { text = '🐞' })
+
+dap.listeners.before.attach.dapui_config = function()
+  ui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  ui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  ui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  ui.close()
+end
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { 'jay-babu/mason-nvim-dap.nvim' },
+  {
+    'mfussenegger/nvim-dap',
+    event = 'VeryLazy',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+      'jay-babu/mason-nvim-dap.nvim',
+      'theHamsta/nvim-dap-virtual-text',
+    },
+  },
+}
